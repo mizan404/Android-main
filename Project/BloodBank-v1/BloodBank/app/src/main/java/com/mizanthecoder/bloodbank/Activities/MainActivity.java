d07a6bdce2769f4b246958538c233791a3659bc4
Muhammad Mizanur Rahman <55780952+mizan404@users.noreply.github.com>
2021-02-09 18:47:58 +0600

    09.02.2021_class

---
+++ Project/BloodBank-v1/BloodBank/app/src/main/java/com/mizanthecoder/bloodbank/Activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mizanthecoder.bloodbank.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mizanthecoder.bloodbank.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
